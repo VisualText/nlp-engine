38deb7bf8784f66925b4860966ccd9773045e851
David de Hilster <david.dehilster@lexisnexisrisk.com>
2021-10-12 20:17:25 -0400

    NLP-ENGINE-197 Fix CMakeLists.txt in nlp to fetch tag correctly
    Signed-off-by: David de Hilster <david.dehilster@lexisnexisrisk.com>

--- nlp/makeversion.bat
+++ nlp/makeversion.bat
@@ -6,12 +6,11 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
      set year=%%l
 )
 set datestr=%month%/%day%/%year%
-FOR /F "tokens=*" %%F IN ('git.exe describe --tags') DO (
-SET version=%%F
+FOR /F "tokens=*" %%F IN ('git.exe tag ^| sort -V ^| tail -1') DO (
+     set version=%%F
 )
-SET version=v1.8.4
 echo // This file was created on %datestr%
 echo // Do not edit - do not add to git
 echo char const *Version() {
-echo     return "%version%";
+echo     return "v1.9.2";
 echo }
